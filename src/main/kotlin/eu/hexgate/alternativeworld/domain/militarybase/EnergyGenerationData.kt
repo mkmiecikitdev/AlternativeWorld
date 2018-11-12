505fb5f2dbf3dc4a5804f846864e3a16963f6803
Bambz <mich.kmiecik@gmail.com>
2018-11-12 13:22:33 +0100

    military base factory

--- src/main/kotlin/eu/hexgate/alternativeworld/domain/militarybase/EnergyGenerationData.kt
+++ src/main/kotlin/eu/hexgate/alternativeworld/domain/militarybase/EnergyGenerationData.kt
@@ -2,10 +2,10 @@ package eu.hexgate.alternativeworld.domain.militarybase
 
 data class EnergyGenerationData(
         val value: Int,
-        val rate: Int
+        val rate: Float
 ) {
 
-    fun nextLevel() = EnergyGenerationData(value * rate, rate)
+    fun nextLevel() = EnergyGenerationData((value * rate).toInt(), rate)
 
     fun calculateGeneration(weatherRate: Float) = (weatherRate * value).toInt()
 
--- src/main/kotlin/eu/hexgate/alternativeworld/domain/militarybase/MilitaryBaseFactory.kt
+++ src/main/kotlin/eu/hexgate/alternativeworld/domain/militarybase/MilitaryBaseFactory.kt
@@ -1,14 +1,41 @@
 package eu.hexgate.alternativeworld.domain.militarybase
 
+import io.vavr.collection.HashMap
+import java.time.Duration
+
 class MilitaryBaseFactory {
 
     fun create(userId: Long): MilitaryBase {
         return MilitaryBase(
                 userId,
+                EnergyBalance.init(),
                 RawMaterials.init(),
                 Coordinates.initWithRandomValues(),
-                EnergyBalance.init()
-        )
+                initBuildings()
+                )
     }
+
+    private fun initBuildings() =
+            Buildings(
+                    HashMap.of(
+                            BuildingType.SOLAR_POWER_STATION, createSolarPowerStation(),
+                            BuildingType.WIND_FARM, createWindFarm()
+                    )
+            )
+
+
+    private fun createSolarPowerStation() =
+            Generator(
+                    BasicBuildingData.init(
+                            CryptocurrencyPriceData(25, 1.2f),
+                            UpgradingTimeData(Duration.ofMinutes(4), 1.5f)),
+                    EnergyGenerationData(100, 1.2f))
+
+    private fun createWindFarm() =
+            Generator(
+                    BasicBuildingData.init(
+                            CryptocurrencyPriceData(30, 1.22f),
+                            UpgradingTimeData(Duration.ofMinutes(6), 1.6f)),
+                    EnergyGenerationData(120, 1.15f))
 
 }
